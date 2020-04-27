9a77bb10a43cf19c8f08d3103c75d856088acfa8
Dean <dpantages@gmail.com>
2020-04-27 09:34:17 -0400

    Added basic connections to demonstrate proof of concept for QProcess

---
+++ opensslcontroller.cpp
@@ -0,0 +1,6 @@
+#include "opensslcontroller.h"
+
+OpenSSLController::OpenSSLController()
+{
+
+}
---
+++ opensslcontroller.h
@@ -0,0 +1,13 @@
+#ifndef OPENSSLCONTROLLER_H
+#define OPENSSLCONTROLLER_H
+
+#include <QObject>
+
+class OpenSSLController
+{
+    Q_OBJECT
+public:
+    OpenSSLController();
+};
+
+#endif // OPENSSLCONTROLLER_H
